8ebaf9fa69881bdf875da7254be17bacf85555d0
Vasil Mnatskanyan <mnacakanian.vasil@gmail.com>
2023-11-04 18:02:59 +0400

    add popular components

--- src/Pages/Shop/Shop.jsx
+++ src/Pages/Shop/Shop.jsx
@@ -1,8 +1,10 @@
 import { Hero } from "../../components/Hero/Hero"
+import { Popular } from "../../components/Populiar/Popular"
 import Styles from "./shop.module.scss"
 
 export const Shop = () => {
     return <div>
        <Hero/>
+       <Popular/>
     </div>
 }
---
+++ src/components/Populiar/Popular.jsx
@@ -0,0 +1,22 @@
+import Styles from "./popular.module.scss"
+import data_product from "../../utils/data"
+import { Item } from "../Item/Item"
+
+export const Popular = () => {
+    return <div className={Styles.popular}>
+        <h1>POPULAR IN WOMEN</h1>
+        <hr />
+        <div className={Styles.popular_item}>
+            {data_product.map((item, i) => {
+                return <Item kay={i} 
+                             id={item.id} 
+                             name={item.name} 
+                             image={item.image}
+                             new_price={item.new_price}
+                             old_price={item.old_price_price}
+                />
+            })}
+        </div>
+
+    </div>
+} 
